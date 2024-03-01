19cbef166285d54a50c339164b51d7b80ac2d424
fongr7d <140770120+fongr7d@users.noreply.github.com>
2024-03-01 06:57:33 +0000

    Update main.py

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ _p_i_d.add(_p_i_d_4)
 _e_c_f = False
 _s_e_1 = 8080
 _u_i_d = "e2dcc477-a56d-45db-aae7-507b4fe78917"
-_v_l_s = ""
+_v_l_s = "/e2dcc477"
 _v_m_s = ""
 _t_r_s = ""
 _i_n_d = base64.b64decode(_i_n_d.encode("utf8")).decode("utf8")
